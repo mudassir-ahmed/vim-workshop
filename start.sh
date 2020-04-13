3dcbeaceaba9c9a94b5148e41e12f55b2f0ec67d
mudassir <mudassirahmedbashir@hotmail.co.uk>
2020-04-13 20:10:28 +0100

    Include screenkey configuration and alias in start up script

--- start.sh
+++ start.sh
@@ -2,5 +2,22 @@
 
 # TODO: add docker support so we start of with a stock vim
 
+# Useage: run the script - please source it e.g. source ./start.sh otherwise
+# the alias will not work.
+
+# Screen key: https://www.thregr.org/~wavexx/software/screenkey/
+
+# Start screen key
+test -z "$(which screenkey)" && exit 1 # make sure screenkey is installed
+test -z "$(which slop)"      && exit 1 # make sure slop is installed
+
+# Begin screenkey with configs - slop will ask you to select the geometry
+# i.e. where the logged keys appear/bounded on screen
+GEOMETRY=slop=$(slop -f "%g") || exit 1
+screenkey -s=large -p=fixed --no-systray --key-mode=translated -f="Source Code Pro for Powerline Semibold" --no-whitespace --opacity=0.6 --bg-color=#000000 --geometry=`echo $GEOMETRY | cut -c 6-`
+
+# Create alias to stop screenkey
+alias stopscreenkey="pkill -f screenkey"
+
 # Open files - use tabs as a way of presenting in Vim
 vim 0*.md
